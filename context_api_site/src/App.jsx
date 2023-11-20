5dd1cdb17c51b04b52c69e4d14d5bd937d1c569a
vijaykrishn55 <119862236+vijaykrishn55@users.noreply.github.com>
2023-11-20 18:57:29 +0000

    part 2

--- context_api_site/src/App.jsx
+++ context_api_site/src/App.jsx
@@ -1,17 +1,36 @@
+import { useEffect, useState } from "react";
+import { ThemeProvider } from "./context/Theme";
+import Card from "./components/Card";
+import Btn from "./components/Btn";
+
 export default function App() {
+  const [ThemeMode,setThemeMode]=useState("light")
+  const darkMode=()=>{
+    setThemeMode("light")
+  }
+  const lightMode=()=>{
+    setThemeMode("dark")
+  }
+
+  useEffect(()=>{
+    const html=document.querySelector("html")
+    html.classList.remove("dark","light")
+    html.classList.add(ThemeMode)
+  },[ThemeMode])
+
   return (
-    
-<div className="flex flex-wrap min-h-screen items-center">
-                <div className="w-full">
-                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        
-                    </div>
+    <ThemeProvider value={{ThemeMode,darkMode,lightMode}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <Btn/>
+          </div>
 
-                    <div className="w-full max-w-sm mx-auto">
-                       
-                    </div>
-                </div>
-            </div>
-
-  )
+          <div className="w-full max-w-sm mx-auto">
+            <Card/>
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
 }
